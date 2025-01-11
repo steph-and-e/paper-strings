1067cdc6f85c216762722bcc4588ffa00b8b5a79
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-01-11 11:57:14 -0500

    working on navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar';
+
 function App() {
     return (
         <>
-            <h1>hi</h1>
+            <Navbar />
         </>
     );
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className='w-full h-16 bg-white shadow-lg shadow-black/25'>
+            <div>hi</div>
+        </nav>
+    );
+}
