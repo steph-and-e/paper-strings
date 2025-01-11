34e9295e1cf5be698545a743b276d51039222f52
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-01-11 11:30:06 -0500

    ok set up tailwind config content

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
