647de02c0b52c34a468f3990dc478d041565ca19
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-01-11 11:41:35 -0500

    added colour palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'pur-light': '#E787FC',
+                'pur-med': '#BF5BF3',
+                'pur-dark': '#4E1595',
+                'yel-light': '#FFC144',
+                'yel-med': '#FF9F0A',
+                'yel-dark': '#C15B02',
+            },
+        },
     },
     plugins: [],
 };
