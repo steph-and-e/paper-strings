e8a50e1b829be05136a145b9cf47e05a33aa8a83
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-01-11 12:45:00 -0500

    Ok figured out tailwind 4.0

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,9 @@
 export default function Hero() {
     return (
         <div className='w-screen h-screen flex flex-col  items-center justify-center'>
-            <h1 className='text-8xl mb-6 font-bold'>Paper Strings</h1>
+            <h1 className='text-8xl font-stretch-ultra-expanded mb-6 font-bold'>
+                Paper Strings
+            </h1>
             <h2 className='text-2xl'>
                 Accessibility in the arts, all with a single fold!
             </h2>
